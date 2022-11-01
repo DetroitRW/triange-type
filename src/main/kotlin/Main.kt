9fc43fce53a81078f9f5a718282fd578e1b54a78
Ilya Brant <detroitib31@gmail.com>
2022-11-01 12:14:34 +0600

    Написал код, для ввода данных "Пользователем"

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,18 @@
+import java.util.*
+
 fun main(args: Array<String>) {
-    println("")
+
     val reader = Scanner(System.`in`)
+
+    print("Введите длину отрезка 1: ")
+    val отрезак1 = reader.nextDouble()
+
+    print("Введите длину отрезка 2: ")
+    val отрезок2 = reader.nextDouble()
+
+    print("Введите длину отрезка 3: ")
+    val отрезок3 = reader.nextDouble()
+
 
 
 
